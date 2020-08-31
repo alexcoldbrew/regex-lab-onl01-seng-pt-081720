48304b8c619cf1691fb0d5afd192981db916c1e2
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-31 20:02:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.match(/\b\w[0-9]{10}\b/)
+  #!!phone.match(/\b\w[0-9]{10}\b/)
+  !!phone.match(/^([0-9\(\)\/\+ \-]*)$/)
 end
